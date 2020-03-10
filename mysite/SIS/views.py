b1cec534e3782909865d025d8d0bd0157da02488
mzhang <mzhang@brontecollege.ca>
2020-03-10 12:45:22 -0400

    added more pages

--- mysite/SIS/views.py
+++ mysite/SIS/views.py
@@ -6,13 +6,27 @@ from django.urls import reverse
 from .models import Student, Locker, Teacher, Courses, Assignment, Marks
 from django.views import generic
 
-def index(request):
+def studentIndex(request):
     student_list = Student.objects.all()
     template = loader.get_template('students/index.html')
     context = {
         'student_list': student_list,
     }
     return HttpResponse(template.render(context, request))
+def index(request):
+    return render(request, 'homepage/index.html',context=None)
+
+def addStudent(request):
+    return render(request, 'students/addStudent.html',context=None)
+
+def listStudents(request):
+    student_list = Student.objects.all()
+    template = loader.get_template('students/studentList.html')
+    context = {
+        'student_list': student_list,
+    }
+    return HttpResponse(template.render(context, request))
+
 def detail(request, student_id):
     try:
         student = Student.objects.get(pk=student_id)
@@ -44,3 +58,30 @@ def confirmStudent(request, student_id):
             s = Student(id=request.POST['id'], grade=request.POST['grade'], name=request.POST['fname'], surname=request.POST['lname'],locker_id=student.locker_id)
             s.save()
             return HttpResponseRedirect(reverse('SIS:detail', args=(request.POST['id'],)))
+
+def addStudentConfirm(request):
+    if(request.POST['id'] == '' or request.POST['fname'] == '' or request.POST['lname'] == '' or request.POST['grade'] == ''):
+            return render(request, 'students/addStudent.html', {
+            'error_message': "One of your fields are empty",
+            })
+    else:
+        try:
+            student = Student.objects.get(pk=request.POST['id'])
+            if(student.id > 0):
+                return render(request, 'students/addStudent.html', {
+                'error_message': "This student already exists",
+                })
+        except Student.DoesNotExist:
+            try:
+                locker = Locker.objects.get(pk=request.POST['locker'])
+                if(locker.id >= 0):
+                    s = Student(id=request.POST['id'], grade=request.POST['grade'], name=request.POST['fname'], surname=request.POST['lname'],locker_id=request.POST['locker'])
+                    s.save()
+                    return render(request, 'students/addStudent.html', {
+                    'error_message': "Successfully added",
+                    })
+            except Locker.DoesNotExist:
+                return render(request, 'students/addStudent.html', {
+                'error_message': "The locker doesn't exist, please create one first",
+                })
+                
